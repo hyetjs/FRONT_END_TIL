9f1f902e7d03794cd59f895212f6505df42dcdf3
hyetjs <cherryade98@gmail.com>
2023-09-29 23:22:46 +0900

    commit

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -6,36 +6,65 @@
 
 변수 a는 main 함수 안에서만 사용된다
 변수 b는 해당 js 어디에서든 사용된다
-변수 c는 메인 함수 안에서 실행되는 콜백함수 solution의 인자이다
+변수 c는 메인 함수 안에서 실행되는 콜백함수 solution의 인자(매개변수)이다
 
 변수 d는 solution에서 전달받은 c를 다른 형태로 바꿔주는 임의의 함수이며 solution 함수의 반환 값이다.
 변수 e는 main함수의 최종 반환 값으로 향후 다른 함수(parse)에서 재사용된다.
 */
 
-// 콜백 함수로 만들려면 main 함수에서 solution 함수를 인자로 받고,
-// main 함수 내에서 solution 함수를 호출할 때 해당 함수를 호출하는 대신 콜백 함수로 넘겨야 합니다.
-// 아래는 이를 구현한 예제 코드입니다
-
 // let b = 10;
 
-// function main(a,sol) {
-//   return solution(a + 2);
-// }
-
-// function solution(c) {
-//   function d() {
-//     return c + b;
+// function main(a) {
+//     return solution(a + 2);
 //   }
-//   return d();
-// }
 
-// let e = main(3);
+//   function solution(c) {
+//     function d() {
+//       return c + b;
+//     }
+//     return d();
+//   }
 
-// function parse(f) {
-//   return e + f;
-// }
+//   let e = main(3);
 
-// console.log(parse(3));
+//   function parse(f) {
+//     return e + f;
+//   }
+
+//   console.log(parse(3));
+
+let b = 10;
+
+function main(a) {
+  let e = a + solution(7);
+  function solution(c) {
+    let d = (c,b) => {
+        c + b
+        return d();
+    }
+  }
+  return e;
+}
+
+console.log(main(5));
+
+// // 숫자 배열을 순회하면서 각 숫자에 대해 콜백 함수 호출
+// function forEachNumber(arr, callback) {
+//     for (let i = 0; i < arr.length; i++) {
+//       callback(arr[i]);
+//     }
+//   }
+
+//   // 콜백 함수 정의
+//   function processNumber(number) {
+//     console.log(`숫자 ${number}를 처리합니다.`);
+//   }
+
+//   // 숫자 배열
+//   const numbers = [1, 2, 3, 4, 5];
+
+//   // forEachNumber 함수에 콜백 함수 전달하여 사용
+//   forEachNumber(numbers, processNumber);
 
 // let b = 10;
 
@@ -52,35 +81,3 @@
 // }
 
 // console.log(main(3, solution));
-
-// 비동기 함수 예제: 1초 후에 "작업 완료"를 출력하는 함수
-// function doAsyncTask(callback) {
-
-//     setTimeout(function() {
-//       console.log("작업 완료");
-//       callback(); // 콜백 함수 호출
-//     }, 1000);
-//   }
-
-//   // 비동기 작업 수행
-//   console.log("작업 시작");
-
-//   doAsyncTask(function() {
-//     console.log("콜백 함수 호출");
-//   });
-
-//   console.log("다음 작업");
-
-// 숫자를 더하는 함수
-function add(a, b, callback) {
-  const result = a + b;
-  callback(result); // 콜백 함수 호출
-}
-
-// 콜백 함수 정의
-function printResult(result) {
-  console.log("결과:", result);
-}
-
-// add 함수 호출
-add(5, 3, printResult);
---
+++ test.js
@@ -0,0 +1,13 @@
+function Circle(radius) {
+    // this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+    this.radius = radius;
+}
+
+Circle.prototype.getDiameter = function () {
+    // this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+    return 2 * this.radius;
+};
+
+// 생성자 함수로 인스턴스를 생성하려면 먼저 생성자 함수를 정의해야 한다.
+const circleInstance = new Circle(5);
+console.log(circleInstance.getDiameter()); // 10
