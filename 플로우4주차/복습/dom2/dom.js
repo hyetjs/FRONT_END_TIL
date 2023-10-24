dc37b6353e879c8bef4904bff3547eb269a389a5
hyetjs <cherryade98@gmail.com>
2023-10-24 23:58:43 +0900

    풀이추가

--- 플로우4주차/복습/dom2/dom.js
+++ 플로우4주차/복습/dom2/dom.js
@@ -1,8 +1,53 @@
-import mockPost from './mock.json' assert { type: 'json' };
+import mockPost from "./mock.json" assert { type: "json" };
 console.log(mockPost);
 
-const $postDetail = document.querySelector('#post-detail');
-const $repliesList = document.querySelector('#replies-list');
+const $postDetail = document.querySelector("#post-detail");
+const $repliesList = document.querySelector("#replies-list");
+const $button = document.querySelector("button");
+const $input = document.querySelector("input");
+
+const postTitle = document.createElement("h3");
+postTitle.textContent = mockPost.post.title;
+
+const postContent = document.createElement("p");
+postContent.textContent = mockPost.post.content;
+
+const postWriter = document.createElement("span");
+postWriter.textContent = mockPost.post.User.nickName;
+
+$postDetail.appendChild(postTitle);
+$postDetail.appendChild(postContent);
+$postDetail.appendChild(postWriter);
+
+const renderReply = (replies) => {
+  const replyUser = document.createElement("li");
+  replyUser.textContent = replies.User.nickName;
+
+  const replyContent = document.createElement("p");
+  replyContent.textContent = replies.content;
+
+  $repliesList.appendChild(replyUser);
+  replyUser.appendChild(replyContent);
+};
+
+for (let post of mockPost.post.Replies) {
+  renderReply({
+    ...post,
+  });
+}
+
+$button.addEventListener("click", () => {
+  if (!$input.value.trim()) {
+    return alert("내용을 입력해주세요");
+  }
+  const newReply = {
+    User: { nickName: "비회원" },
+    content: $input.value,
+  };
+  mockPost.post.Replies.push(newReply);
+  renderReply(newReply);
+  $input.value = "";
+});
 
 /* 
     import(참조)한 json data를
